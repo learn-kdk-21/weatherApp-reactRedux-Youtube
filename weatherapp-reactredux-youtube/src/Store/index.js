80199b0d902db2f5c394fc4968520e21775fe2f0
learn-21-k <kapadiakeval2194@gmail.com>
2024-06-25 23:13:45 +0530

    final changes

---
+++ weatherapp-reactredux-youtube/src/Store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import weatherReducer from "./Slices/WeatherSlice";
+const store = configureStore({
+  reducer: {
+    weather: weatherReducer,
+  },
+});
+export default store;
---
+++ weatherapp-reactredux-youtube/src/config/config.js
@@ -0,0 +1,2 @@
+export const hostName = "https://api.openweathermap.org";
+export const appId = "542c720093303c1b33185f7ac9e1b3ba";
